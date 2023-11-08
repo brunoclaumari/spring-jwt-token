0136f61677229bece1c071eeb9c634c2dec43a9d
brunoclaumari <brunoclaumari@gmail.com>
2023-11-08 00:17:04 -0300

    Início implementação JWT

---
+++ src/main/java/com/security/jwt/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.security.jwt.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.security.jwt.demo.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+    
+    boolean existsByUsername(String username);
+}
---
+++ src/test/java/com/security/jwt/demo/DioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jwt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
